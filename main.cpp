172c0c6642e2a8a3816ac5c1c27599e6507b53e0
0mar <omsrichardson@gmail.com>
2018-04-19 12:54:33 +0200

    added command line arguments

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@
 
 using Eigen::ArrayXXf;
 
-int main() {
-    Simulation sim = Simulation(100, 100);
+int main(int argc, char **argv) {
+    int nx = 100;
+    int ny = 100;
+    if (argc==3) {
+        nx = (int)strtol(argv[1],nullptr,0);
+        ny = (int)strtol(argv[2],nullptr,0);
+    }
+    std::cout << nx << ny << std::endl;
+    Simulation sim = Simulation(nx, ny);
     sim.run();
 }
